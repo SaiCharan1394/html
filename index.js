db36bbe73a71cb4be6837bc9f9b58f96e94509e3
SaiCharan1394 <105579195+SaiCharan1394@users.noreply.github.com>
2023-08-08 11:21:34 +0530

    changes

---
+++ index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const heading=React.createElement('h1',{},'SAICHARAN');
+const root= ReactDOM.createRoot(document.getElementById('left'));
+root.render(heading);
